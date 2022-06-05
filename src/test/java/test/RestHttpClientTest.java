a0fcce877a9711b3ae5549a68fa0e6b938c2d184
kaelamon <kaelamon20@gmail.com>
2022-06-05 14:15:35 +0300

    Баловался

--- src/test/java/test/RestHttpClientTest.java
+++ src/test/java/test/RestHttpClientTest.java
@@ -3,31 +3,36 @@ package test;
 
 
 import api.integrationSubsystems.perfomance.models.Cars;
-import api.integrationSubsystems.perfomance.models.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.*;
 import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
+import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DisplayName("Рест запросы через Apache HTTP")
 public class RestHttpClientTest {
-    @Test
-    void exampleTest() throws Exception {
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpget = new HttpGet("http://77.50.236.203:4880/cars");
-        System.out.println("Request Type: "+httpget.getMethod());
-        HttpResponse httpresponse = httpclient.execute(httpget);
-        Scanner sc = new Scanner(((CloseableHttpResponse) httpresponse).getEntity().getContent());
 
-        //System.out.println(httpresponse.getStatusLine());
-        while(sc.hasNext()) {
-            System.out.println(sc.nextLine());
-        }
+    @Test
+    void exampleTest() throws Exception {
+
+        HttpResponse httpresponse = httpclient.execute(httpget);
+        String content = EntityUtils.toString(((CloseableHttpResponse) httpresponse).getEntity());
+        ObjectMapper objectMapper = new ObjectMapper();
+        Cars[] cars = objectMapper.readValue(content, Cars[].class);
+
+        step("Проверим что массив Cars не пустой", () -> {assertNotNull(cars);});
+
+        step("Проверим что массив Cars имеет что-то в себе", () -> {assertTrue(cars.length > 0);});
 
     }
 
